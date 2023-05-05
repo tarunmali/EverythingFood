85cd2029ee3624645a236dfa61350797cab3af82
Tarun Mali <tarunmalibackup2@gmail.com>
2023-05-05 18:53:46 +0530

    q

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const AppLayou5t = () =>  (
+    <div class="app">
+        <h1> App</h1>
+    </div>
+)
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayou5t/>);
